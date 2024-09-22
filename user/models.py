3fec6dc951c059915be3310d946db5a5493b158c
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-22 14:55:53 +0530

    feat: create user django db model - Created migration for the same

--- user/models.py
+++ user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, db_index=True, primary_key=True
+    )
+    name = models.CharField(max_length=255, null=False, blank=False)
+    contact_number = models.CharField(max_length=255, null=False, blank=False)
+    aadhar_number = models.CharField(max_length=255, null=False, blank=False)
+    pan_number = models.CharField(max_length=255, null=False, blank=False)
+    is_setup_completed =  models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'users'
