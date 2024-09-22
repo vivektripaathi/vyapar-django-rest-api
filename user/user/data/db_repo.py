b4eec225294d9264501753ffb518274edbdf50c6
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-22 15:06:59 +0530

    feat(data): implement get method in UserDbRepository. - Added the 'get' method to 'UserDbRepository' for retrieving a user by 'id' and converting it to a 'UserDomainModel'  using Pydantic's 'from_orm'. - Defined the abstract method in 'UserDbAbstractRepository' to ensure consistency across repository implementations.

--- user/user/data/db_repo.py
+++ user/user/data/db_repo.py
@@ -1,4 +1,12 @@
+from user.models import User
+from user.types import UserId
 from user.user.data.abstract_repo import UserAbstractRepository
+from user.user.domain.domain_models import UserDomainModel
 
 class UserDbRepository(UserAbstractRepository):
     """User Database Repository."""
+
+    def get(self, id: UserId) -> UserDomainModel:
+        """Get user by id"""
+        user_db_entry = User.objects.get(id=id)
+        return UserDomainModel.from_orm(user_db_entry)
