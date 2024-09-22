bae5041abdc1ff30a1cc8a094f93b1a8340fca44
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-22 16:29:13 +0530

    feat(url): add get user endpoint

--- user/user/presentation/urls.py
+++ user/user/presentation/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 
+from user.user.presentation.views import GetUserView
 
-urlpatterns = []
+
+urlpatterns = [
+    path(
+        "<uuid:id>/",
+        GetUserView.as_view(),
+        name = "get_user"
+    )
+]
