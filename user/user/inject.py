74b220cb0b22effe472aa848a3294f690e64e652
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-22 16:23:56 +0530

    injected get user use case

--- user/user/inject.py
+++ user/user/inject.py
@@ -2,9 +2,11 @@ from dependency_injector import containers, providers
 
 from user.user.data.abstract_repo import UserAbstractRepository
 from user.user.data.db_repo import UserDbRepository
+from user.user.domain.use_cases.get_user_use_case import GetUserUseCase
 
 class UserSubAppContainer(containers.DeclarativeContainer):
     db_repo = providers.Dependency(
         instance_of=UserAbstractRepository,
         default=UserDbRepository(),
     )
+    get_user_use_case = providers.Factory(GetUserUseCase)
