406ee3a0b750a87072976a6496faa1df6e20ab72
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-25 11:08:06 +0530

    type(user): gave request type as VyaparAuthenticatedHttpRequest

--- user/user/presentation/views.py
+++ user/user/presentation/views.py
@@ -1,20 +1,17 @@
-# import pydantic
 from dependency_injector.wiring import Provide
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from user.types import UserId
+from core.request_types import VyaparAuthenticatedHttpRequest
 from user.user.domain.use_cases.get_user_use_case import GetUserUseCase
 
 
 class GetUserView(APIView):
     def get(
         self,
-        request,
-        id: UserId,
+        request: VyaparAuthenticatedHttpRequest,
         get_user_use_case: GetUserUseCase = Provide["user.get_user_use_case"],
     ):
-        # print("got here")
-        response = get_user_use_case.execute(id)
+        response = get_user_use_case.execute(request.user.id)
         return Response(response.dict_serialized(), status=status.HTTP_200_OK)
