f7f62c878525cae8401dd4382ca6c749298c6e38
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-25 11:11:42 +0530

    feat(core): Add an utility to decode jwt token

--- core/utils.py
+++ core/utils.py
@@ -1,8 +1,12 @@
 from typing import Dict, Union, cast
 
+import jwt
 import orjson
+from django.conf import settings
 from pydantic import BaseModel
 from pydantic.generics import GenericModel
+
+from core.exceptions import InvalidTokenException, TokenExpiredException
 
 
 def _dict_serialized(obj: Union[BaseModel, GenericModel], *args, **kwargs) -> Dict:
@@ -19,3 +23,12 @@ class VyaparBaseModel(BaseModel):
 
     def dict_serialized(self, *args, **kwargs) -> Dict:
         return _dict_serialized(self, *args, **kwargs)
+
+
+def decode_jwt_token(token) -> dict:
+    try:
+        return jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+    except jwt.ExpiredSignatureError as exc:
+        raise TokenExpiredException from exc
+    except Exception as exc:
+        raise InvalidTokenException from exc
