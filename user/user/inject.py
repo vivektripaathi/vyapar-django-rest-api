d9a2e9a032c8a5801a12519d3a58085755edef19
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-25 14:16:46 +0530

    chore: inject send_otp_use_case into user sub app container

--- user/user/inject.py
+++ user/user/inject.py
@@ -3,6 +3,7 @@ from dependency_injector import containers, providers
 from user.user.data.abstract_repo import UserAbstractRepository
 from user.user.data.db_repo import UserDbRepository
 from user.user.domain.use_cases.get_user_use_case import GetUserUseCase
+from user.user.domain.use_cases.send_otp_use_case import SendOTPUseCase
 
 
 class UserSubAppContainer(containers.DeclarativeContainer):
@@ -11,3 +12,4 @@ class UserSubAppContainer(containers.DeclarativeContainer):
         default=UserDbRepository(),
     )
     get_user_use_case = providers.Factory(GetUserUseCase)
+    send_otp_use_case = providers.Factory(SendOTPUseCase)
