2ceb591410c45dbb2a1487ef1e80ccef61c13559
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-25 14:15:21 +0530

    refactor: send otp use case

--- user/user/domain/use_cases/send_otp_use_case.py
+++ user/user/domain/use_cases/send_otp_use_case.py
@@ -1,19 +1,28 @@
-from django.http import JsonResponse
-from .models import User
 import random
-import jwt
-from django.conf import settings
-from datetime import datetime, timedelta, timezone
+
+from dependency_injector.wiring import Provide
+
+from core.token.domain.use_cases.create_token_use_case import CreateTokenUseCase
 
 
 class SendOTPUseCase:
-    def execute(self, phone) -> str:
-        otp = random.randint(1000, 9999)
-        data = {
+    def __init__(
+        self,
+        create_token_use_case: CreateTokenUseCase = Provide[
+            "token.create_token_use_case"
+        ],
+    ) -> None:
+        self.create_token_use_case = create_token_use_case
+
+    def _get_token_request(self, phone: str) -> dict:
+        otp: int = random.randint(1000, 9999)
+        return {
             "phone": phone,
             "otp": otp,
-            "exp": datetime.now(tz=timezone.utc) + timedelta(minutes=10),  # Token expires in 10 minutes
         }
-        encoded_jwt = jwt.encode(data, settings.JWT_SECRET, algorithm="HS256")
-        # send_otp_to_phone(phone, otp) #Abstract Function
+
+    def execute(self, phone: str) -> str:
+        token_request = self._get_token_request(phone)
+        encoded_jwt = self.create_token_use_case.execute(token_request)
+        # TODO: send_otp_to_phone(phone, otp) #Abstract Function
         return encoded_jwt
