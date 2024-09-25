bae1b4a2266d24f2e3285dec68d9a8a169e5ad61
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-25 11:13:08 +0530

    feat(user): add '_get' method to get user efficiently from db

--- user/user/data/db_repo.py
+++ user/user/data/db_repo.py
@@ -1,5 +1,6 @@
 import logging
 
+from user.exceptions import UserNotFound
 from user.models import User
 from user.types import UserId
 from user.user.data.abstract_repo import UserAbstractRepository
@@ -11,8 +12,14 @@ logger = logging.getLogger(__name__)
 class UserDbRepository(UserAbstractRepository):
     """User Database Repository."""
 
+    def _get(self, user_id: UserId) -> User:
+        try:
+            return User.objects.get(id=user_id)
+        except User.DoesNotExist as exc:
+            raise UserNotFound from exc
+
     def get(self, id: UserId) -> UserDomainModel:
         """Get user by id"""
         logger.info("Getting user by id %s", id)
-        user_db_entry = User.objects.get(id=id)
+        user_db_entry = self._get(user_id=id)
         return UserDomainModel.from_orm(user_db_entry)
