740ec104568bb93b1cc30e1e795dd4b96c9360e8
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-26 13:52:26 +0530

    add wrong otp exception

--- user/exceptions.py
+++ user/exceptions.py
@@ -7,3 +7,9 @@ class UserNotFound(VyaparException):
     code = "Vyapar_USER_0001"
     status_code = status.HTTP_404_NOT_FOUND
     default_detail = "Requested user does not exist"
+
+
+class WrongOTPException(VyaparException):
+    code = "Vyapar_USER_0002"
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = "The provided OTP is incorrect"
