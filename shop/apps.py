0f995bd1e31f04658a3520e582e6d55ae7eabadb
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-28 22:00:22 +0530

    wireed shop app container with shop apps

--- shop/apps.py
+++ shop/apps.py
@@ -4,3 +4,14 @@ from django.apps import AppConfig
 class ShopConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "shop"
+    inject_container = None
+
+    def ready(self) -> None:
+        from shop import shop
+        from shop.inject import ShopContainer
+
+        self.inject_container = ShopContainer()
+        self.inject_container.wire(
+            packages=[shop],
+            modules=[],
+        )
